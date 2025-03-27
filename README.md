b8065b59cfcc922e51455e22d5269b626674d7bf
Abi Franklin <abifranklin@gmail.com>
2025-03-27 14:13:08 -0500

    Fundamentals Module 1 complete

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # PythonFundamentals
-Installed cocommit
---
+++ taxcalculator.py
@@ -0,0 +1,5 @@
+print('Hello World')
+
+amount = float(input('Enter the amount: '))
+tax = float(input('Enter the tax rate: '))
+print('The total is: ', amount + amount * tax)
